b5e649ba0ab4e03eb8148ba424f42710939cd545
Blake Mumford <blake.mumford@indoctrinate.com.au>
2017-08-25 15:37:57 +1000

    Initial work on dermal SPA Added bootstrap 4 Upgraded packages Changes to Scss

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
-import { HomeComponent } from './home/home.component';
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {PatientDashboardComponent} from './patient-dashboard/patient-dashboard.component';
 
 const appRoutes: Routes = [
-{path: 'home', component: HomeComponent}
+  {path: 'home', component: HomeComponent},
+  {path: 'patients', component: PatientDashboardComponent}
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(appRoutes)],
-    exports: [RouterModule]
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,28 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { CallbackComponent } from './callback/callback.component';
+import { PatientDashboardComponent } from './patient-dashboard/patient-dashboard.component';
+import { PatientListItemComponent } from './patient-list-item/patient-list-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    CallbackComponent
+    CallbackComponent,
+    PatientDashboardComponent,
+    PatientListItemComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule.forRoot()
   ],
   providers: [AuthService],
   bootstrap: [AppComponent]
